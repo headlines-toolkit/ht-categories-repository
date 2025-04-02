e559b30d34d7b4d787035fc590f0c62826adb905
fulleni <fulleni@hotmail.com>
2025-04-02 08:22:37 +0100

    docs: update package description

--- lib/ht_categories_repository.dart
+++ lib/ht_categories_repository.dart
@@ -1,4 +1,5 @@
-/// A Very Good Project created by Very Good CLI.
+/// A Dart package providing a repository implementation for managing
+/// news categories, acting as an abstraction layer over a HtCategoriesClient.
 library;
 
 export 'src/ht_categories_repository.dart';
