ca52dfa3bc17d8c94ce5f823cfba4c55be0c88cc
fulleni <fulleni@hotmail.com>
2025-04-02 08:07:42 +0100

    chore: initial commit
    - Added basic project structure
    - Setup CI/CD workflow
    - Created initial library

---
+++ lib/src/ht_categories_repository.dart
@@ -0,0 +1,7 @@
+/// {@template ht_categories_repository}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class HtCategoriesRepository {
+  /// {@macro ht_categories_repository}
+  const HtCategoriesRepository();
+}
---
+++ test/src/ht_categories_repository_test.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ht_categories_repository/ht_categories_repository.dart';
+
+void main() {
+  group('HtCategoriesRepository', () {
+    test('can be instantiated', () {
+      expect(HtCategoriesRepository(), isNotNull);
+    });
+  });
+}
